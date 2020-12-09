13cb6e9453c255b5ff9a8c4aab2bdad21aeb44f6
GitStux <63031851+GitStux@users.noreply.github.com>
2020-12-09 15:50:08 -0800

    testing git

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>temporary</h1>
-    </div>
+  return (<div className = "App">
+
+
+    <h1> Temporary Lol </h1> </div >
   );
 }
 
