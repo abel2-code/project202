b268357a83b5cc3990df1e8265f6dfc2755adf5e
GitStux <63031851+GitStux@users.noreply.github.com>
2020-12-11 14:46:02 -0800

    added isAuthenticated

---
+++ src/components/AuthStatus.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthenticationButton from "./Authentication-button";
+
+const AuthStatus = () => (
+  <div>
+    <AuthenticationButton />
+  </div>
+);
+
+export default AuthStatus;
