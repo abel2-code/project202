f09b7f820a5ae5a2d9b6bbabdcbc0e791334188a
Abelardo Miranda <available_email_address@yahoo.com>
2020-12-16 07:40:03 -0600

    backend work

--- back-end/index.js
+++ back-end/index.js
@@ -10,46 +10,47 @@ server.listen(process.env.PORT || 9000);
 
 const notes = [];
 
-
 server.get("/", (req, res) => {
-    res.send("API working");
+  res.send("API working");
 });
 
 server.get("/notes", (req, res) => {
-    res.send(notes);
+  res.send(notes);
 });
 
 server.post("/notes", (req, res) => {
-    notes.push(req.body);
-    res.send(notes);
+  notes.push(req.body);
+  res.send(notes);
 });
-
 
 server.put("/notes/:student", (req, res) => {
-    const student = req.params.student;
-    const note = req.body;
-    let result = notes.filter(note => note.user.student === student);
+  const student = req.params.student;
+  const note = req.body;
+  let result = notes.filter((note) => note.user.student === student);
 
+  if (result.length !== 0) {
     result[0].user.notes = req.body.notes;
-    console.log(result[0])
-    res.send(result[0])
+    res.send(result[0]);
+  } else {
+    return res.status(404).send("Student not found");
+  }
 });
 
-server.delete("/notes/:student", (req, res) =>{
-    const student = req.params.student;
-    let noteIdx = -1;
-    
-    notes.map((note, idx) => {
-        if(note.user.student.toLowerCase() === student.toLowerCase()){
-            noteIdx = idx;
-            return;
-        }
-    })
-    if(noteIdx === -1){
-        return res.status(404).send("Student not found");
+server.delete("/notes/:student", (req, res) => {
+  const student = req.params.student;
+  let noteIdx = -1;
+
+  notes.map((note, idx) => {
+    if (
+      note.user.student.toLowerCase().trim() === student.toLowerCase().trim()
+    ) {
+      noteIdx = idx;
+      return;
     }
-    notes.splice(noteIdx, 1);
-    res.send({success: "Success"});
-    
-    })
-    
+  });
+  if (noteIdx === -1) {
+    return res.status(404).send("Student not found");
+  }
+  notes.splice(noteIdx, 1);
+  res.send({ success: "Success" });
+});
